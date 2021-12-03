7c2f3b800ce523e13e873bf953ebc3617e6e71ec
JkjustinK <bkang01@risd.edu>
2021-12-03 14:11:50 -0500

    edits

--- timelineproj/final/counter.js
+++ timelineproj/final/counter.js
@@ -1,5 +1,5 @@
-var seconds=0
-let minutes=0
+var seconds=0;
+		let minutes=0;
 
 
 function counter(){
@@ -9,20 +9,7 @@ function counter(){
 
 window.setInterval(counter,1000);
 
-function showtime(){
-	setTimeout(function () {document.getElementById('frame1').style.visibility='visible'}, 0);
-	setTimeout(function () {document.getElementById('frame1').style.visibility='hidden'}, 10000);
-	setTimeout(function () {document.getElementById('frame2').style.visibility='visible'}, 10000);
-	setTimeout(function () {document.getElementById('frame2').style.visibility='hidden'}, 20000);
-	setTimeout(function () {document.getElementById('frame3').style.visibility='visible'}, 20000);
-	setTimeout(function () {document.getElementById('frame3').style.visibility='hidden'}, 30000);
-	setTimeout(function () {document.getElementById('frame4').style.visibility='visible'}, 30000);
-	setTimeout(function () {document.getElementById('frame4').style.visibility='hidden'}, 40000);
-	setTimeout(function () {document.getElementById('frame5').style.visibility='visible'}, 40000);
-	setTimeout(function () {document.getElementById('frame5').style.visibility='hidden'}, 9999000);
-}
 
-/*
 function showtime(){
 	setTimeout(function () {document.getElementById('frame1').style.visibility='visible'}, 0);
 	setTimeout(function () {document.getElementById('frame1').style.visibility='hidden'}, 120000);
@@ -34,15 +21,14 @@ function showtime(){
 	setTimeout(function () {document.getElementById('frame4').style.visibility='hidden'}, 114000000);
 	setTimeout(function () {document.getElementById('frame5').style.visibility='visible'}, 114000000);
 	setTimeout(function () {document.getElementById('frame5').style.visibility='hidden'}, 999999999999);
-*/
+}
 
 if(seconds == 0){
-	showtime()
+	showtime();
 }
 
 
 if (seconds >= 3) {
-    var element = document.getElementById("frame1");
-        element.style.opacity = "0.2";
+    var element = document.getElementById("frame1").style.visibility= 'hidden';
 };
     
